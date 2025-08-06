627d0169e07a7072fb713b4cd0830a64159b903e
parhambt <parhambosi@gmail.com>
2025-08-06 09:51:55 +0330

    implement Place_lamp views

--- Places_Lamp/views.py
+++ Places_Lamp/views.py
@@ -1,10 +1,53 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import PlaceForm , HomeForm , LampForm ,RoomForm
 # Create your views here.
-class AddInfo(View) : 
-    def post(self) : 
-        pass
 
+class SettingsView(LoginRequiredMixin, View):
+    template_name = 'settings.html'
 
-    def get(self):
-        pass
+    def get(self, request):
+        return render(request, self.template_name, {
+            'place_form': PlaceForm(),
+            'home_form': HomeForm(),
+            'room_form': RoomForm(),
+            'lamp_form' : LampForm()
+        })
+
+    def post(self, request):
+        place_form = PlaceForm()
+        home_form = HomeForm()
+        room_form = RoomForm()
+        lamp_form = LampForm()
+
+        if 'submit_place' in request.POST:
+            place_form = PlaceForm(request.POST)
+            if place_form.is_valid():
+                place=place_form.save(commit=False)
+                place.owner = request.user
+                place.save()
+                pass
+
+        elif 'submit_home' in request.POST:
+            home_form = HomeForm(request.POST)
+            if home_form.is_valid():
+                home_form.save()
+                
+
+        elif 'submit_room' in request.POST:
+            room_form = RoomForm(request.POST)
+            if room_form.is_valid():
+                room_form.save()
+
+        elif 'submit_lamp' in request.POST:
+            lamp_form = RoomForm(request.POST)
+            if lamp_form.is_valid():
+                lamp_form.save()
+
+        return render(request, self.template_name, {
+            'place_form': place_form,
+            'home_form': home_form,
+            'room_form': room_form,
+            'lamp_form' : lamp_form
+        })
