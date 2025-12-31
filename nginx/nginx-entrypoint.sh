212abccd9c0b9fc504cd584630a114e7f8c343e9
sina.moradi <sina.moradi@zoodfood.com>
2025-12-31 10:21:18 +0330

    fix minor issue

--- nginx/nginx-entrypoint.sh
+++ nginx/nginx-entrypoint.sh
@@ -5,13 +5,9 @@
 # Check if SSL certificates exist
 if [ -f /etc/letsencrypt/live/node.lilms.top/fullchain.pem ]; then
     echo "SSL certificates found, using HTTPS configuration"
-    # Copy HTTPS config to default.conf
     cp /etc/nginx/nginx-https.conf /etc/nginx/conf.d/default.conf
-    # Remove init config to avoid conflicting server names
-    rm -f /etc/nginx/conf.d/default-init.conf
 else
     echo "SSL certificates not found, using initial HTTP-only configuration"
-    # Copy init config as default
-    cp /etc/nginx/conf.d/default-init.conf /etc/nginx/conf.d/default.conf
+    cp /etc/nginx/nginx-init.conf /etc/nginx/conf.d/default.conf
 fi
 
